e60d9fab6a0d78d706497369f13371ca902ba648
Frederico Andrade de Carvalho <andradalho@gmail.com>
2023-04-25 15:56:43 -0300

    Inserindo primeiras funções

--- teste.py
+++ teste.py
@@ -1,3 +1,12 @@
 def cria_conta(numero, titular, saldo, limite):
     conta = {"número": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
+
+def deposita(conta, valor):
+    conta ["saldo"] += valor
+
+def deposita(conta, valor):
+    conta ["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
